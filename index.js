a69f12a5e8ce30327407e31fb2218cf0c33b0e2a
Md Hamdan Nasir <nasirmdham@gmail.com>
2026-01-08 23:51:49 +0530

    new

--- index.js
+++ index.js
@@ -4,12 +4,15 @@ const path = require("path");
 
 app.use(express.json());
 app.use(express.urlencoded({extended : true}));
-app.use(express.static(path.join(__dirname,`public`)));
-app.set("view engine", "ejs");  
+app.use(express.static(path.join(__dirname,"public")));
+app.set("view engine","ejs");
 
-app.get("/:username",(req,res)=>{
-    res.send(req.params.username);
+app.get("/",(req,res)=>{
+    res.send("hello")
+})
+app.get("/profile/:username/:age",(req,res)=>{
+    res.send("papa "+req.params.username+" of age "+req.params.age   );
+    // res.send();
 })
 
-
-app.listen(3000,()=>console.log("chal gaya"))
+app.listen(3000);
