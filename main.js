f7394637b754219925799ac06df14a1bf401ab2d
Md Hamdan Nasir <nasirmdham@gmail.com>
2026-01-15 12:57:13 +0530

    new

--- main.js
+++ main.js
@@ -1,13 +1,20 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const route = require('./routes/route');
 
-app.use("/",(req,res,next)=>{
-    console.log("hello babay hamdan")
+// app.use(express.static("public"));
+app.use("/route",route)
+app.use("/about",(req,res,next)=>{
+    console.log("about section wala middleware chal rha hai")
     next();
 })
 app.get("/", (req, res) => {
     res.send("Hello Papa");
+})
+
+app.get("/about",(req,res)=>{
+    res.send("aa gayila about section me")
 })
 
 app.listen(port, () => {
--- routes/route.js
+++ routes/route.js
@@ -1,8 +1,15 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/route",(req,res)=>{
+router.use((req, res, next) => {
+    console.log("router middleware running");
+    next();
+})
+router.get("/", (req, res) => {
     res.send("route is running");
+})
+router.get("/about", (req, res) => {
+    res.send("about is running");
 })
 
 module.exports = router;
