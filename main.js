61e3ee7eee17344aed471dfa57c14c8994ecff3c
Md Hamdan Nasir <nasirmdham@gmail.com>
2026-01-17 20:26:12 +0530

    new

--- main.js
+++ main.js
@@ -3,7 +3,12 @@ const app = express();
 const port = 3000;
 const route = require('./routes/route');
 
-// app.use(express.static("public"));
+app.use(express.static(Path2D.join(__dirname,"public")));
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+
+
 app.use("/route",route)
 app.use("/about",(req,res,next)=>{
     console.log("about section wala middleware chal rha hai")
