5f148351811e3cda5c27db302c15f64beb40c643
Md Hamdan Nasir <nasirmdham@gmail.com>
2026-01-19 00:07:44 +0530

    practicing mongodb

---
+++ back.mongodb.js
@@ -0,0 +1,69 @@
+use("newDB");
+
+db.createCollection("peoples")
+
+db.peoples.insertMany([
+    {
+  name: "hamdan",
+  age: 22,
+  height: 5.5
+},
+
+{
+  name: "ali",
+  age: 24,
+  height: 5.8
+},
+
+{
+  name: "sara",
+  age: 21,
+  height: 5.4
+},
+
+{
+  name: "rahul",
+  age: 23,
+  height: 5.7
+},
+
+{
+  name: "ayesha",
+  age: 20,
+  height: 5.3
+},
+
+{
+  name: "faizan",
+  age: 25,
+  height: 5.9
+},
+
+{
+  name: "neha",
+  age: 22,
+  height: 5.6
+},
+
+{
+  name: "imran",
+  age: 26,
+  height: 5.8
+},
+
+{
+  name: "pooja",
+  age: 21,
+  height: 5.4
+},
+
+{
+  name: "arjun",
+  age: 24,
+  height: 5.10
+}
+
+])
+
+let a = db.peoples.find({name:"neha"});
+console.log(a);
